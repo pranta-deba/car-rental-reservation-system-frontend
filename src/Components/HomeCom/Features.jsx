948af8cccf4225636050a6b7afcbaa4c00bac5fe
Pranto Deb <rajprit0m@outlook.com>
2025-03-01 04:20:47 +0600

    [Honme] : features page

---
+++ src/Components/HomeCom/Features.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Features = () => {
+    return (
+        <div className='container mx-auto'>
+            <div>
+                <div></div>
+                <div>
+                    <img className='w-[30%]' src="./png5.webp" alt="Image....." />
+                </div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Features;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../../Components/HomeCom/Banner';
 import SectionHeader from '../../Components/SectionHeader/SectionHeader';
+import Features from '../../Components/HomeCom/Features';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <SectionHeader titleSplit={true} title='PXrent Features'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             </SectionHeader>
+            <Features />
         </div>
     );
 };
