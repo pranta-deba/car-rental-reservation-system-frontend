df89605452f318e6c914f3a7835be9b63593638c
Pranto Deb <rajprit0m@outlook.com>
2025-03-01 04:13:10 +0600

    [Section Header]

---
+++ src/Components/SectionHeader/SectionHeader.jsx
@@ -0,0 +1,17 @@
+
+const SectionHeader = ({ title = 'Title', children = 'Sub Title', titleSplit = false }) => {
+    return (
+        <div className="container mx-auto text-center my-4 md:my-6 lg:my-10 space-y-3">
+            <h1 className="text-4xl md:text-5xl font-bold drop-shadow">{
+                titleSplit ?
+                    <>
+                        <span>{title.split(' ')[0]} </span>
+                        <span className="primary-text">{title.split(' ')[1]}</span>
+                    </> : <span>{title}</span>
+            }</h1>
+            <p className="md:w-2/3 mx-auto text-gray-500">{children}</p>
+        </div>
+    );
+};
+
+export default SectionHeader;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import Banner from '../../Components/HomeCom/Banner';
+import SectionHeader from '../../Components/SectionHeader/SectionHeader';
 
 const Home = () => {
     return (
         <div className='m-0 p-0'>
             <Banner />
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repudiandae voluptas fugit vel nulla porro adipisci debitis neque accusamus tenetur. At aperiam voluptate expedita officiis ipsam animi, quas consectetur? Quibusdam!
-                Odit nostrum fuga odio sit consequuntur, culpa molestiae, repellat mollitia hic error distinctio consequatur est ducimus laudantium laboriosam, sed illum sapiente delectus id omnis nemo. Accusamus harum excepturi quae officia!
-                Debitis porro maxime, minima vitae libero praesentium in ab reiciendis nostrum tempore nemo eius, modi perferendis excepturi velit, odio fugit officiis ipsa! Aliquid, amet. Ullam ad vero libero commodi molestiae.
-                Laudantium fugiat culpa maxime quo, id corrupti, consequuntur quos aspernatur non reprehenderit repellat numquam aliquid quisquam corporis voluptatibus veniam voluptatum natus dolor soluta nemo sed molestias. Provident nam tenetur aperiam.
-                Ut, consectetur pariatur est autem quo minus hic eum suscipit. Omnis impedit eligendi ducimus error cumque. Officiis, doloremque sapiente optio amet, voluptatibus eligendi laboriosam aliquid non deserunt sunt, ab officia!
-                Quasi sed sapiente dicta rerum fugit dolorem cum amet fugiat esse quod architecto alias ipsum adipisci est asperiores veniam quidem vitae earum, sequi quam corporis. Laborum id sunt sint accusantium.
-                Accusamus modi magni quae aspernatur nihil, quasi sequi aliquid, laborum sint eaque adipisci nesciunt numquam maiores doloribus? Quaerat, veniam aspernatur deserunt voluptatibus consequuntur, ut quibusdam dolores vel excepturi delectus voluptas!
-                Dicta ducimus quo pariatur laudantium, esse praesentium rem deleniti, aliquid at eligendi nihil voluptatem facere provident, perferendis odit tenetur. Deleniti maiores soluta, cupiditate alias nulla consequatur dolorum animi officiis dolore!
-                Iste amet voluptatibus, harum aliquid iusto debitis! Nobis magnam quibusdam reiciendis natus modi expedita delectus, beatae mollitia enim! Optio nesciunt nihil ad. Omnis, repellat! Molestiae autem minima quos sequi suscipit?
-                Quae facilis blanditiis et labore fuga necessitatibus iusto repudiandae. Et at, accusantium laboriosam necessitatibus, praesentium eligendi itaque libero, repellendus dolores veniam ipsum. Id fuga sit iure! Mollitia quam consequuntur sapiente?
-            </div>
+            <SectionHeader titleSplit={true} title='PXrent Features'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </SectionHeader>
         </div>
     );
 };
