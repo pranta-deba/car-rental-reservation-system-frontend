ae741e90a39df14cef97a173cdc2edbc818e19a5
Pranto Deb <rajprit0m@outlook.com>
2025-03-01 03:14:48 +0600

    [auth] : sign in page complete

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import './index.css'
 import { router } from './Routers/Routers.jsx'
 import { RouterProvider } from 'react-router-dom'
 import AppProvider from './Providers/AppProvider.jsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppProvider>
       <RouterProvider router={router} />
+      <Toaster
+        position="top-left"
+        reverseOrder={false}
+      />
     </AppProvider>
   </StrictMode>,
 )
