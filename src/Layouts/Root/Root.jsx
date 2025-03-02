3dc7129ac4f3e66dde0814ca9f0922e7b8550136
Pranto Deb <rajprit0m@outlook.com>
2025-03-02 19:45:07 +0600

    [Loader] user loader

--- src/Layouts/Root/Root.jsx
+++ src/Layouts/Root/Root.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import Navbar from '../../Components/Shared/Navbar';
 import Footer from '../../Components/Shared/Footer';
 import { Outlet } from 'react-router-dom';
+import useGetContext from '../../Hooks/UseContext/useGetContext';
+import Loader from '../../Components/Loader/Loader';
 
 const Root = () => {
+    const { userLoader } = useGetContext();
+
+    if (userLoader) return <div className='flex justify-center items-center min-h-screen w-full'><Loader size={"4xl"} /></div>;
+
+
     return (
         <>
             <Navbar />
