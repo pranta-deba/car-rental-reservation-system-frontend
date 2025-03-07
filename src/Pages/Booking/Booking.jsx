07c3072ac5b02290fb768e8db76d7a30215ae680
Pranto Deb <rajprit0m@outlook.com>
2025-03-07 20:58:15 +0600

    [Detail] car details OK

--- src/Pages/Booking/Booking.jsx
+++ src/Pages/Booking/Booking.jsx
@@ -62,7 +62,7 @@ const Booking = () => {
                     {bookings.map((booking) => (
                         <div key={booking._id} className="border border-gray-300 p-4 rounded-lg shadow-md">
                             <h2 className="text-lg font-bold mb-2">{booking.car.name}</h2>
-                            <p className="text-gray-600 mb-2">{new Date(booking.date).toLocaleDateString()} || {booking.startTime} - {booking.endTime || 'Ongoing'}</p>
+                            <p className="text-gray-600 mb-2">{new Date(booking.date).toLocaleDateString()} || {booking.startTime} - {booking.endTime || <span className='text-[#FF6E00]'>Ongoing</span>}</p>
                             <p className="text-gray-600 mb-2">Total Cost: ${booking.totalCost}</p>
                             <p className="text-gray-600 mb-2">Address: {booking.user.address}</p>
                             <img src={booking.car.image} alt={booking.car.name} className="rounded-lg mt-2 h-52 w-full object-cover object-center" style={{ maxWidth: '100%' }} />
