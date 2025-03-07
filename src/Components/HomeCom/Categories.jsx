f3eff5ba499001aac12c9002f8e2507d903c34df
Pranto Deb <rajprit0m@outlook.com>
2025-03-07 19:47:19 +0600

    [Categories]

---
+++ src/Components/HomeCom/Categories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SectionHeader from '../SectionHeader/SectionHeader';
+
+
+const Categories = () => {
+    return (
+        <div className='container mx-auto my-8'>
+            <SectionHeader titleSplit={true} title='Vehicle Categories'>
+                Exploring Innovation: Cutting-Edge Vehicles of Tomorrow
+            </SectionHeader>
+        </div>
+    );
+};
+
+export default Categories;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import Cars from '../../Components/HomeCom/Cars';
 import { CarDataContext } from '../../Providers/CarDataProvider';
 import BookingModal from '../../Components/Modals/BookingModal';
 import Stat from '../../Components/HomeCom/Stat';
+import Categories from '../../Components/HomeCom/Categories';
 
 const Home = () => {
     const { cars, loader, handleSearch, handleSort } = useContext(CarDataContext);
@@ -16,6 +17,7 @@ const Home = () => {
             <Cars cars={cars} carsLoader={loader} handleSort={handleSort} />
             <Features />
             <Stat />
+            <Categories />
         </div>
     );
 };
